c2eb0888cc50cd442704aaced395d9619a9e5e30
Phillip Black <phil@Phillips-MacBook-Air.local>
2022-10-10 18:24:14 -0500

    Added eventlisteners, and created handleMove Funct.

--- js/main.js
+++ js/main.js
@@ -13,25 +13,22 @@ wordsArr = ['adult', 'anger', 'agree',
             'radio', 'scope', 'title',
             'unity', 'value', 'while'];
 
- 
 /*----- state variables -----*/
 
 let winner;
-//constains word chosen from words array.
-let randWord;
-let curWord;
+let randWord; //contains the random word selected from words array. 
+let curWord; // contains array of each letter from randWord.
+let currentSelect; //contains current letter, selected by the event listener. 
 let turnsRemain;
 let wrongPieces;
 let correctPeices;
-let currentSelect;
 
 /*----- cached elements  -----*/
 let msgBox = document.getElementById('msgBox');
 let correctLetters = document.getElementById('correctLetters');
 
-
 /*----- event listeners -----*/
-
+document.getElementById('availLetters').addEventListener('click', handleMove);
 
 /*----- functions -----*/
 
@@ -40,6 +37,16 @@ init();
 
 function init() {
     chooseWord();
+    render();
+}
+
+function handleMove(evt) {
+    currentSelect = evt.target;
+    // Test to see if current selection is in curWord.
+
+}
+
+function render() {
     renderBoard();
 }
 
@@ -47,11 +54,11 @@ function renderBoard() {
     //takes rand word, splits to array of letters. 
     curWord = randWord.split('');
     console.log(curWord)
-    // for Each loop placing leters one by one onto board#corrects letters.
+    // binds letters from curWord to spot on the board.
     curWord.forEach(function(letter, idx) {
         correctLetters.innerHTML += `<div id="${idx}">${letter.toUpperCase()}</div>`;
     })
-    // display none for letters. 
+    
 }
 
 // Choose a random word, from wordsArr, and return value. 
