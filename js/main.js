e20b9eafc188fc90600ce193e1c720c6d8fa4600
Phillip Black <phil@Phillips-MacBook-Air.local>
2022-10-11 18:30:54 -0500

    renderMove function now renders all moves correctly.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,35 @@
 
 /*----- constants -----*/
+
 wordsArr = ['adult', 'anger', 'agree', 
-            'aline', 'begin', 'brave',
-            'child', 'class', 'clock', 
-            'dance', 'dream', 'drive',
-            'earth', 'entry', 'event',
-            'feild', 'faith', 'focus',
-            'glass', 'group', 'green',
-            'index', 'input', 'judge',
-            'level', 'model', 'novel',
-            'order', 'peace', 'queen',
-            'radio', 'scope', 'title',
-            'unity', 'value', 'while'];
+'aline', 'begin', 'brave',
+'child', 'class', 'clock', 
+'dance', 'dream', 'drive',
+'earth', 'entry', 'event',
+'feild', 'faith', 'focus',
+'glass', 'group', 'green',
+'index', 'input', 'judge',
+'level', 'model', 'novel',
+'order', 'peace', 'queen',
+'radio', 'scope', 'title',
+'unity', 'value', 'while'];
 
 /*----- state variables -----*/
 
-let winner;
 let randWord; //contains the random word selected from words array. 
 let curWord; // contains array of each letter from randWord.
 let currentSelect; //contains current letter, selected by the event listener. 
-let turnsRemain; // contatins the remaining turns before player loses. 
+let wrongGuess; // contatins the remaining turns before player loses. 
 let wrongLtrs; // contains the letter of a wrong selection
-let correctPeices;
+let correctGuess; //contains array of correct letters chosen by user. 
+let isWinner;
+let isLoser;
 
 /*----- cached elements  -----*/
 let msgBox = document.getElementById('msgBox');
 let correctLetters = document.getElementById('correctLetters');
+let spaceman = document.getElementById('spaceman');
+wrongLtrs = document.getElementById('incorrectLtrs');
 
 /*----- event listeners -----*/
 document.getElementById('availLetters').addEventListener('click', handleMove);
@@ -38,19 +42,23 @@ init();
 function init() {
     chooseWord();
     render();
-    turnsRemain = 6;
-    winner = false;
+    wrongGuess = 0;
+    correctGuess = 0;
+    isWinner = false;
+    isLoser = false;
 }
 
 function handleMove(evt) {
     currentSelect = evt.target;
-    console.log(currentSelect)
-    
     renderMove();
 }
 
 function checkForWinner () {
-    
+    return correctGuess === randWord.length;
+}
+
+function checkForLoser () {
+
 }
 
 function render() {
@@ -58,13 +66,28 @@ function render() {
 }
 
 function renderMove() {
-    wrongLtrs = document.getElementById('incorrectLtrs');
+ // put gaurd here.   
+    document.getElementById(`${currentSelect.innerText}`).style.display = 'none';
+    
     curWord.forEach(function(curLetter, idx) {
         if(currentSelect.innerText === curLetter.toUpperCase()) {
             document.getElementById(`${idx}`).style.display = 'grid';
             document.getElementById(`${idx}`).style.gridArea = `1 / ${idx + 1}`;
+            msgBox.innerText = 'CORRECT GUESS';
+            correctGuess += 1;
         }    
     });
+    
+    let select = currentSelect.innerText.toLowerCase();
+    let isRight = curWord.includes(select);
+    
+    if(isRight === false) {
+        wrongLtrs.append(`${currentSelect.innerText}`);
+        msgBox.innerText = 'INCORRECT GUESS';
+        msgBox.style.fontSize = '30px';
+        wrongGuess += 1;
+        spaceman.src = `img/spaceman-${wrongGuess}.jpg`;
+    }
 }
 
 function renderBoard() {
@@ -75,8 +98,6 @@ function renderBoard() {
     curWord.forEach(function(letter, idx) {
         correctLetters.innerHTML += `<div id="${idx}">${letter.toUpperCase()}</div>`;
     })
-    //tests if current letter selection is equal to a letter in 
-    // correct word array. 
 }
 
 // Choose a random word, from wordsArr, and return value. 
