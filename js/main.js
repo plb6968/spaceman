3818639fba9837e3200284a89894b4242c1fcdde
Phillip Black <phil@Phillips-MacBook-Air.local>
2022-10-11 13:14:23 -0500

    renderMove function now renders correct move to the board.

--- js/main.js
+++ js/main.js
@@ -19,8 +19,8 @@ let winner;
 let randWord; //contains the random word selected from words array. 
 let curWord; // contains array of each letter from randWord.
 let currentSelect; //contains current letter, selected by the event listener. 
-let turnsRemain;
-let wrongPieces;
+let turnsRemain; // contatins the remaining turns before player loses. 
+let wrongLtrs; // contains the letter of a wrong selection
 let correctPeices;
 
 /*----- cached elements  -----*/
@@ -38,16 +38,33 @@ init();
 function init() {
     chooseWord();
     render();
+    turnsRemain = 6;
+    winner = false;
 }
 
 function handleMove(evt) {
     currentSelect = evt.target;
-    // Test to see if current selection is in curWord.
+    console.log(currentSelect)
+    
+    renderMove();
+}
 
+function checkForWinner () {
+    
 }
 
 function render() {
     renderBoard();
+}
+
+function renderMove() {
+    wrongLtrs = document.getElementById('incorrectLtrs');
+    curWord.forEach(function(curLetter, idx) {
+        if(currentSelect.innerText === curLetter.toUpperCase()) {
+            document.getElementById(`${idx}`).style.display = 'grid';
+            document.getElementById(`${idx}`).style.gridArea = `1 / ${idx + 1}`;
+        }    
+    });
 }
 
 function renderBoard() {
@@ -58,7 +75,8 @@ function renderBoard() {
     curWord.forEach(function(letter, idx) {
         correctLetters.innerHTML += `<div id="${idx}">${letter.toUpperCase()}</div>`;
     })
-    
+    //tests if current letter selection is equal to a letter in 
+    // correct word array. 
 }
 
 // Choose a random word, from wordsArr, and return value. 
